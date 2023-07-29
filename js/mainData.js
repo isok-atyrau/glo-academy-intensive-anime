6b100d927ff5dcd4f47cf09f83ee91569b3c9603
isok.atyrau <islambek.brgaliyev@gmail.com>
2023-07-29 10:51:33 +0500

    First commit

--- js/mainData.js
+++ js/mainData.js
@@ -1,8 +1,29 @@
 const mainData = () => {
-   //    const API_URL = './db.json'
-
-   // ДЗ
    const API_URL = "https://test-abec8-default-rtdb.firebaseio.com/anime.json"
+
+   const renderTopAnime = (array) => {
+      const wrapper = document.querySelector(".filter__gallery")
+
+      wrapper.innerHTML = ""
+      array.forEach((item) => {
+         wrapper.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+               <div class="ep">${item.rating} / 10</div>
+               <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+               <h5><a href="/anime-details.html">${item.title}</a></h5>
+            </div>`
+         )
+
+         wrapper.querySelectorAll(".set-bg").forEach((elem) => {
+            elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+         })
+      })
+   }
+
+   const renderAnimeList = (array, ganres) => {
+      console.log(ganres)
+   }
 
    fetch(API_URL)
       .then((responce) => {
@@ -13,7 +34,12 @@ const mainData = () => {
          }
       })
       .then((data) => {
-         console.log(data)
+         const ganres = new Set()
+
+         data.forEach((item) => ganres.add(item.ganre))
+
+         renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5))
+         renderAnimeList(data, ganres)
       })
       .catch((error) => {
          console.dir(error)
